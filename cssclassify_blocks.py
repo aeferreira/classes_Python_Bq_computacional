0fc63271b5bf349baa28e048f1376371348316ee
aeferreira <ferreiae@gmail.com>
2019-07-30 15:00:55 +0100

    Changes in cssclassify_block.py

--- cssclassify_blocks.py
+++ cssclassify_blocks.py
@@ -1,11 +1,25 @@
 from pathlib import Path
 
-mks = sorted(Path('.').joinpath('docs').glob('*.md'))
+mks = sorted(Path().joinpath('docs').glob('*.md'))
 
+print('--------- Backing up files --------------')
+backup = Path() / 'backup'
+backup.mkdir(exist_ok=True)
 for p in mks:
-    print('processing', p)
+    dest = backup / p.name
+    print(f'- copying {p} to {dest}')
+    content = p.read_text()
+    #print(content.count('``` python\n'))
+    #content = content.replace('``` python\n', '``` python3\n')
+    with dest.open('w') as f:
+        f.write(content)
+
+print('---------- Processing files ------------')
+for p in mks:
+    dest = backup / p.name
+    print(f'- processing {p}')
     content = p.read_text()
     print(content.count('``` python\n'))
     content = content.replace('``` python\n', '``` python3\n')
-    with p.open('w') as f:
+    with dest.open('w') as f:
         f.write(content)
