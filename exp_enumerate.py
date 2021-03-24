fab5080c2d6e35d35819b1dcbe9c21f59ccbc0f0
aeferreira <ferreiae@gmail.com>
2021-03-24 22:27:16 +0000

    Deployed c93c736 with MkDocs version: 1.1.2

--- exp_enumerate.py
+++
@@ -1,5 +0,0 @@
-seq = 'ADKHLILTAVGGCWFHVAFWEVEKAGAHKWE'
-
-for i, aminoacido in enumerate(seq):
-    if aminoacido in 'KL':
-        print(i, ':', aminoacido)
--- exppath.py
+++
@@ -1,3 +0,0 @@
-from pathlib import Path
-
-print(Path.cwd())
